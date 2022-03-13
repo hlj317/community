fe14366cce29809e2559b9a5d8127af781116809
hlj317 <huanglijun317@126.com>
2022-03-13 15:58:05 +0800

    伤病名单更新

--- src/biz/common/data/mydata_vote_2.js
+++ src/biz/common/data/mydata_vote_2.js
@@ -1062,6 +1062,94 @@ export const myData = [
                 homeRatio:51
             }
         ]
+    },
+
+    //3月14日
+    {
+        date: 'Mon Mar 14',
+        list: [
+            {
+                time:'Mon 3/14, 1:00 AM',
+                awayName:'knicks',
+                awayLeftPoints:'+5.5',
+                awayRightPoints:'+6.0',
+                awayRatio:43,
+                homeName:'nets',
+                homeLeftPoints:'-5.5',
+                homeRightPoints:'-6.0',
+                homeRatio:57
+            },{
+                time:'Mon 3/14, 3:00 AM',
+                awayName:'clippers',
+                awayLeftPoints:'-5.0',
+                awayRightPoints:'-5.0',
+                awayRatio:59,
+                homeName:'pistons',
+                homeLeftPoints:'+5.0',
+                homeRightPoints:'+5.0',
+                homeRatio:41
+            },{
+                time:'Mon 3/14, 3:30 AM',
+                awayName:'mavericks',
+                awayLeftPoints:'+5.0',
+                awayRightPoints:'+5.0',
+                awayRatio:39,
+                homeName:'celtics',
+                homeLeftPoints:'-5.0',
+                homeRightPoints:'-5.0',
+                homeRatio:61
+            },{
+                time:'Mon 3/14, 6:00 AM',
+                awayName:'seven6ers',
+                awayLeftPoints:'-13.0',
+                awayRightPoints:'-12.5',
+                awayRatio:56,
+                homeName:'magic',
+                homeLeftPoints:'+13.0',
+                homeRightPoints:'+12.5',
+                homeRatio:44
+            },{
+                time:'Mon 3/14, 7:00 AM',
+                awayName:'grizzlies',
+                awayLeftPoints:'-12.5',
+                awayRightPoints:'-12.5',
+                awayRatio:63,
+                homeName:'thunder',
+                homeLeftPoints:'+12.5',
+                homeRightPoints:'+12.5',
+                homeRatio:37
+            },{
+                time:'Mon 3/14, 7:00 AM',
+                awayName:'pacers',
+                awayLeftPoints:'+11.0',
+                awayRightPoints:'+11.0',
+                awayRatio:38,
+                homeName:'hawks',
+                homeLeftPoints:'-11.0',
+                homeRightPoints:'-11.0',
+                homeRatio:62
+            },{
+                time:'Mon 3/14, 7:00 AM',
+                awayName:'rockets',
+                awayLeftPoints:'+6.0',
+                awayRightPoints:'+5.5',
+                awayRatio:51,
+                homeName:'pelicans',
+                homeLeftPoints:'-6.0',
+                homeRightPoints:'-5.5',
+                homeRatio:49
+            },{
+                time:'Mon 3/14, 9:00 AM',
+                awayName:'lakers',
+                awayLeftPoints:'+8.5',
+                awayRightPoints:'+8.5',
+                awayRatio:44,
+                homeName:'suns',
+                homeLeftPoints:'-8.5',
+                homeRightPoints:'-8.5',
+                homeRatio:56
+            }
+        ]
     }
 
 
